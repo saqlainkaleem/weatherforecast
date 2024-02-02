ed5a303c19a2933afa1c356f26c3468cff7c3ac3
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-02-03 00:25:54 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.css";
+import Weather from "./components/Weather.jsx";
+const App = () => {
+	return (
+		<div className="app">
+			<Weather />
+		</div>
+	);
+};
+
+export default App;
